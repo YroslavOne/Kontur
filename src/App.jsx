b8403c0737f46c6011d74b265f91cf186b1ab630
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2024-05-25 03:59:29 +0700

    8.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import Menu from "./components/menu/Menu";
-import "./App.css";
-import Banner from "./components/banner/Banner";
-import Edication from "./components/education/Edication";
-import Methods from "./components/metods/Methods";
-import Gallery from "./components/gallery/Gallery";
-import Map from "./components/map/Map";
-import Footer from "./components/footer/Footer";
-import { useEffect, useState } from "react";
-import MobMenu from "./components/mobMenu/MobMenu";
-import ListMenu from "./components/mobMenu/listMenu/ListMenu";
-import CallBack from "./components/callBack/callBack";
+import Menu from './components/menu/Menu';
+import './App.css';
+import Banner from './components/banner/Banner';
+import Edication from './components/education/Edication';
+import Methods from './components/metods/Methods';
+import Gallery from './components/gallery/Gallery';
+import Map from './components/map/Map';
+import Footer from './components/footer/Footer';
+import { useEffect, useState } from 'react';
+import MobMenu from './components/mobMenu/MobMenu';
+import ListMenu from './components/mobMenu/listMenu/ListMenu';
+import CallBack from './components/callBack/callBack';
 
 function App() {
-  const [width, setWidth] = useState(window.innerWidth);
+  // const [width, setWidth] = useState(window.innerWidth);
   const [open, setOpen] = useState(false);
   const [callBack, setCallBack] = useState(false);
 
-  useEffect(() => {
-    const handleResize = (event) => {
-      setWidth(event.target.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="main-page">
-      {width > 576 ? <Menu /> : <MobMenu setOpen={setOpen} />}
+      <Menu />
+      <MobMenu setOpen={setOpen} />
       <Banner setCallBack={setCallBack} />
       <Edication />
       <Methods />
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,24 +1,24 @@
-import { links } from "../LinksForMenu";
-import styles from "./Menu.module.css";
+import { links } from '../LinksForMenu';
+import styles from './Menu.module.css';
 
 function Menu() {
   return (
-    <menu>
-      <div className={styles["logo-and"]}>
+    <menu className={styles['menu']}>
+      <div className={styles['logo-and']}>
         <a href="/">
-          <div className={styles["logo"]}></div>
+          <div className={styles['logo']}></div>
         </a>
-        <ul className={styles["ul"]}>
+        <ul className={styles['ul']}>
           {links.map((link) => (
             <li key={link.key}>
-              <a className={styles["a"]} href="/">
+              <a className={styles['a']} href="/">
                 {link.text}
               </a>
             </li>
           ))}
         </ul>
       </div>
-      <a className={styles["phone"]} href="tel:+7 (499) 348 93 96">
+      <a className={styles['phone']} href="tel:+7 (499) 348 93 96">
         +7 (499) 348 93 96
       </a>
     </menu>
